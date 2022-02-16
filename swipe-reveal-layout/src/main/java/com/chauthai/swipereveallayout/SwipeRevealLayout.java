081c8e3c69d1ca0273fd6ab70557a96811b92cab
Niklas Dahlheimer <dahlheimer@tentaclesync.com>
2022-02-16 10:16:47 +0100

    fix child view height
    https://github.com/chthai64/SwipeRevealLayout/issues/30

--- swipe-reveal-layout/src/main/java/com/chauthai/swipereveallayout/SwipeRevealLayout.java
+++ swipe-reveal-layout/src/main/java/com/chauthai/swipereveallayout/SwipeRevealLayout.java
@@ -363,37 +363,52 @@ public class SwipeRevealLayout extends ViewGroup {
         int desiredWidth = 0;
         int desiredHeight = 0;
 
+        int wrapChildHeight = 0;
+        int wrapChildWidth = 0;
+
         // first find the largest child
         for (int i = 0; i < getChildCount(); i++) {
             final View child = getChildAt(i);
+            final LayoutParams childParams = child.getLayoutParams();
             measureChild(child, widthMeasureSpec, heightMeasureSpec);
-            desiredWidth = Math.max(child.getMeasuredWidth(), desiredWidth);
-            desiredHeight = Math.max(child.getMeasuredHeight(), desiredHeight);
+
+            if (childParams.height == LayoutParams.WRAP_CONTENT &&
+                    (mDragEdge == DRAG_EDGE_LEFT || mDragEdge == DRAG_EDGE_RIGHT))
+                wrapChildHeight = Math.max(child.getMeasuredHeight(), wrapChildHeight);
+            else
+                desiredHeight = Math.max(child.getMeasuredHeight(), desiredHeight);
+
+            if (childParams.width == LayoutParams.WRAP_CONTENT &&
+                    (mDragEdge == DRAG_EDGE_TOP || mDragEdge == DRAG_EDGE_BOTTOM))
+                wrapChildWidth = Math.max(child.getMeasuredWidth(), wrapChildWidth);
+            else
+                desiredWidth = Math.max(child.getMeasuredWidth(), desiredWidth);
+
         }
+        if (wrapChildHeight != 0) desiredHeight = wrapChildHeight;
+        if (wrapChildWidth !=0) desiredWidth =wrapChildWidth;
+
         // create new measure spec using the largest child width
         widthMeasureSpec = MeasureSpec.makeMeasureSpec(desiredWidth, widthMode);
-        heightMeasureSpec = MeasureSpec.makeMeasureSpec(desiredHeight, heightMode);
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(desiredHeight, MeasureSpec.EXACTLY);
 
-        final int measuredWidth = MeasureSpec.getSize(widthMeasureSpec);
         final int measuredHeight = MeasureSpec.getSize(heightMeasureSpec);
+        final int measuredWidth = MeasureSpec.getSize(widthMeasureSpec);
 
         for (int i = 0; i < getChildCount(); i++) {
             final View child = getChildAt(i);
             final LayoutParams childParams = child.getLayoutParams();
 
             if (childParams != null) {
-                if (childParams.height == LayoutParams.MATCH_PARENT) {
-                    child.setMinimumHeight(measuredHeight);
+                if (childParams.height != LayoutParams.WRAP_CONTENT) {
+                    childParams.height = measuredHeight;
                 }
 
                 if (childParams.width == LayoutParams.MATCH_PARENT) {
-                    child.setMinimumWidth(measuredWidth);
+                    childParams.width = measuredWidth;
                 }
             }
-
             measureChild(child, widthMeasureSpec, heightMeasureSpec);
-            desiredWidth = Math.max(child.getMeasuredWidth(), desiredWidth);
-            desiredHeight = Math.max(child.getMeasuredHeight(), desiredHeight);
         }
 
         // taking accounts of padding
@@ -409,7 +424,7 @@ public class SwipeRevealLayout extends ViewGroup {
             }
 
             if (widthMode == MeasureSpec.AT_MOST) {
-                desiredWidth = (desiredWidth > measuredWidth) ? measuredWidth : desiredWidth;
+                desiredWidth = Math.min(desiredWidth, measuredWidth);
             }
         }
 
@@ -422,7 +437,7 @@ public class SwipeRevealLayout extends ViewGroup {
             }
 
             if (heightMode == MeasureSpec.AT_MOST) {
-                desiredHeight = (desiredHeight > measuredHeight) ? measuredHeight : desiredHeight;
+                desiredHeight = (desiredHeight > measuredHeight)? measuredHeight : desiredHeight;
             }
         }
 
@@ -1152,4 +1167,6 @@ public class SwipeRevealLayout extends ViewGroup {
         DisplayMetrics metrics = resources.getDisplayMetrics();
         return (int) (dp * ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT));
     }
+
+
 }
