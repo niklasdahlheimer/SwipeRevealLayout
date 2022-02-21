d89189b0cb1642327cd9cb03b711e692072b049a
Niklas Dahlheimer <dahlheimer@tentaclesync.com>
2022-02-21 12:19:55 +0100

    update Demo to use AndroidX

--- demo/src/main/java/com/chauthai/swipereveallayoutdemo/MainActivity.java
+++ demo/src/main/java/com/chauthai/swipereveallayoutdemo/MainActivity.java
@@ -2,12 +2,13 @@ package com.chauthai.swipereveallayoutdemo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
